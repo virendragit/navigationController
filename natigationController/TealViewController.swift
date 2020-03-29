2bc9f609e755e27b9e2e15c90d9f89d1ba05e7fe
Virendra Gupta <vgupta@ivycomptech.com>
2020-03-29 09:19:32 +0530

    first commit

---
+++ natigationController/TealViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TealViewController.swift
+//  natigationController
+//
+//  Created by Virendra Gupta on 3/29/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Teal Screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ natigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  natigationController
+//
+//  Created by Virendra Gupta on 3/29/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
